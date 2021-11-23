45fa74ec228af669d8835f526f339794dd09de56
Sandeep Chhoker <schhoker@reputation.com>
2021-11-23 18:41:19 +0530

    multiply string

--- exercises/facebook/numberToWords/numberToWords.js
+++ exercises/facebook/numberToWords/numberToWords.js
@@ -3,8 +3,56 @@
  * @return {string}
  */
 const numberToWords = function (num) {
-  const map = new Map();
-  map.set(1, 'One');
+    const numbers = {
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine',
+        10: 'Ten',
+        11: 'Eleven',
+        12: 'Twelve',
+        13: 'Thirteen',
+        14: 'Fourteen',
+        15: 'Fifteen',
+        16: 'Sixteen',
+        17: 'Seventeen',
+        18: 'Eighteen',
+        19: 'Nineteen',
+        20: 'Twenty',
+        30: 'Thirty',
+        40: 'Forty',
+        50: 'Fifty',
+        60: 'Sixty',
+        70: 'Seventy',
+        80: 'Eighty',
+        90: 'Ninety',
+        100: 'Hundred',
+        1000: 'Thousand',
+        1000000: 'Million',
+        1000000000: 'Billion'
+    };
+    let n = String(num).length;
+    let result = [];
+    let i = 1;
+    //20001
+    while (i < n) {
+        let p = Math.pow(10, i);
+        let s = 1;
+
+        while (numbers[p] === undefined) {
+            p /= 10;
+            s *= 10;
+        }
+        let t = Math.floor(num / (p * s));
+        result.push(numbers[t]);
+        result.push(numbers[s]);
+        result.push(numbers[p]);
+    }
 };
 
 module.exports = numberToWords;
