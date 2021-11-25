542ed5c15d7801801a57da974f177d7d47cbef1b
sdchhoker <sdchhoker@gmail.com>
2021-11-25 20:16:16 +0530

    remove invalid parentheses

--- exercises/facebook/removeInvalidParentheses/removeInvalidParentheses.js
+++ exercises/facebook/removeInvalidParentheses/removeInvalidParentheses.js
@@ -3,50 +3,42 @@
  * @return {string[]}
  */
 const removeInvalidParentheses = function (s) {
-    let max = -1;
-    const result = [];
+    let min = -1;
+    const result = new Set();
     recurse(s, result, 0, 0);
 
     /**
      * @param {string} s
-     * @param {string[]} result
+     * @param {Set<string>} result
      * @param {number} count
      * @param {number} idx
-     * @return {boolean}
+     * @return {void}
      */
     function recurse(s, result = [], count, idx) {
-        if (s.length === 0 || idx > s.length - 1) return count === 0;
-
-        const char = s.charAt(idx);
-
-        if (char === '(') {
-            if (recurse(s, result, count + 1, idx + 1) && max <= s.length) {
-                max = Math.max(max, s.length);
-                result.push(s);
-                return true;
+        if (s.length <= idx) {
+            if (count === 0 && min <= s.length) {
+                result.add(s);
+                min = Math.max(min, s.length);
             }
-
-            if (recurse(s.substring(1), result, count, idx) && max <= s.length - 1) {
-                max = Math.max(max, s.length - 1);
-                result.push(s.substring(1));
-                return true
-            }
-        } else if (char === ')') {
-            if (recurse(s, result, count - 1, idx + 1) && max <= s.length) {
-                max = Math.max(max, s.length);
-                result.push(s);
-                return false;
-            }
-            if (recurse(s.substring(1), result, count, idx) && max <= s.length - 1) {
-                max = Math.max(max, s.length - 1);
-                result.push(s.substring(1));
-                return true;
-            }
+            return;
         }
-        return false;
-    };
 
-    return result;
+        const char = s[idx];
+        if (char !== '(' && char !== ')') {
+            recurse(s, result, count, idx + 1);
+            return;
+        }
+
+        let n = char === ')' ? count - 1 : count + 1;
+        if (n >= 0) {
+            recurse(s, result, n, idx + 1);
+        }
+
+        s = s.substring(0, idx) + s.substring(idx + 1);
+        recurse(s, result, count, idx);
+    }
+
+    return Array.from(result);
 };
 
 
--- exercises/facebook/removeInvalidParentheses/removeInvalidParenthesis.test.js
+++ exercises/facebook/removeInvalidParentheses/removeInvalidParenthesis.test.js
@@ -1,5 +1,13 @@
 const removeInvalidParentheses = require('./removeInvalidParentheses');
 
 test('test case 1', () => {
-    expect(removeInvalidParentheses('()())()')).toEqual(['()()()', '(())()']);
+  expect(removeInvalidParentheses('()())()')).toEqual(['()()()', '(())()']);
+});
+
+test('test case 2', () => {
+  expect(removeInvalidParentheses(')(')).toEqual(['']);
+});
+
+test('test case 3', () => {
+  expect(removeInvalidParentheses('(a)())()')).toEqual(["(a())()", "(a)()()"]);
 });
